5a533842e5e36de1a597c55b274a8a733a387340
Hina-softwareEngineer <hinakhadim2002@gmail.com>
2021-06-09 21:57:50 +0500

    History.dart structure + style has been completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learn/userProfile.dart';
-import 'history.dart';
+import 'productList.dart';
 
 
 void main() {
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HistoryPage(),
+      home: ProductList(),
     );
   }
 }
